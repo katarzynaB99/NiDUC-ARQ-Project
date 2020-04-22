a5ad1346f8f7ddda57f9e7f84617f7a2a0b360c8
Konjix <Konjix@users.noreply.github.com>
2020-04-22 16:12:27 +0200

    Added Gilbert channel

---
+++ gilbert_channel.m
@@ -0,0 +1,32 @@
+function [output_signal] = gilbert_channel(input_signal,p_dz,p_zd)
+%GILBERT_CHANNEL Funkcja generujaca zaklucenia w sygnale z zadanami
+%                prawdopodobienstwami zmiany stanu
+%   input_signal - sygnal wejsciowy
+%   p_dz - prawdopodobienstwo przejscia ze stanu dobrego do zlego
+%   p_zd - prawdopodobienstwo przejscia ze stanu zlego do dobrego
+%   output_signal - sygnal koncowy z losowymi zakloceniami
+%   state - stan w ktorym jest uklad; 0 -> stan dobry, 1 -> stan zly
+    signal_length = length(input_signal);
+    output_signal = zeros(1, signal_length);
+    state = 0;
+    for i = 1:signal_length
+        if state
+            if input_signal == 1
+                output_signal(i) = 0;
+            else
+                output_signal(i) = 1;
+            end
+            x = randi(100);
+            if x>p_zd
+                state = 0;
+            end
+        else
+            output_signal(i) = input_signal(i);
+            x = randi(100);
+            if x>p_dz
+                state = 1;
+            end
+        end
+    end
+end
+
