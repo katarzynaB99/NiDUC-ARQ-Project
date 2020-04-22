64b0166f71bf270ba145f0a6acb92e7659457beb
Kasia <kasiab2099@gmail.com>
2020-04-22 23:29:37 +0200

    Fixed total_redundancy and channel drop down

--- arq_main.m
+++ arq_main.m
@@ -10,12 +10,12 @@ function [ time_taken, bit_error_rate, total_redundancy] = arq_main( signal_leng
     generated_signal = signal_generator(signal_length);
     encoded_signal = [];
    
-    if code_type == 1
+    if code_type == '1'
         %Bit parzystosci
         encoded_signal = parity_bit_encoder(generated_signal, coding_param);
         total_redundancy = length(encoded_signal) - signal_length;
     else
-        if code_type == 2
+        if code_type == '2'
             %Suma kontrolna
             encoded_signal = control_sum_coder(generated_signal, coding_param);
             total_redundancy = coding_param;
@@ -33,17 +33,17 @@ function [ time_taken, bit_error_rate, total_redundancy] = arq_main( signal_leng
     received_signal = [];
     decoded_signal = [];
     error_number = 0;
-    if channel_type == 1
+    if channel_type == '1'
         %KBS
         while status
             [received_signal, error_number] = KBS_channel(encoded_signal, first_probability);
             bit_error_rate = bit_error_rate + error_number;
             sent_bits_sum = sent_bits_sum + encoded_signal_length;
-            if code_type == 1
+            if code_type == '1'
                 %Bit parzystosci
                 [decoded_signal, error] = parity_bit_decoder(received_signal, coding_param);
             else
-                if code_type == 2
+                if code_type == '2'
                     %Suma kontrolna
                     [decoded_signal, error] = control_sum_decoder(received_signal, coding_param);
                 else
@@ -65,11 +65,11 @@ function [ time_taken, bit_error_rate, total_redundancy] = arq_main( signal_leng
             [received_signal, error_number] = gilbert_channel(encoded_signal, first_probability, second_probability);
             bit_error_rate = bit_error_rate + error_number;
             sent_bits_sum = sent_bits_sum + encoded_signal_length;
-            if code_type == 1
+            if code_type == '1'
                 %Bit parzystosci
                 [decoded_signal, error] = parity_bit_decoder(received_signal, coding_param);
             else
-                if code_type == 2
+                if code_type == '2'
                     %Suma kontrolna
                     [decoded_signal, error] = control_sum_decoder(received_signal, coding_param);
                 else
@@ -91,5 +91,7 @@ function [ time_taken, bit_error_rate, total_redundancy] = arq_main( signal_leng
     disp(time_taken);
     disp(bit_error_rate);
     disp(total_redundancy);
+    disp(code_type);
+    disp(channel_type);
 end
 
