240632bbae4a58aae011ccaed8aff4c9dea10922
Kasia <kasiab2099@gmail.com>
2020-04-22 18:21:22 +0200

    Changed CRC coder

--- crc_coder.m
+++ crc_coder.m
@@ -3,8 +3,9 @@ function [encoded_signal] = crc_coder(signal)
 %   Detailed explanation goes here
 poly = 'z^32 + z^26 + z^23 + z^22 + z^16 + z^12 + z^11 + z^10 + z^8 + z^7 + z^5 + z^4 + z^2 + z + 1';
 crc32 = comm.CRCGenerator('Polynomial', poly);
-
+signal = signal.';
 encoded_signal = crc32(signal);
-
+encoded_signal = encoded_signal.';
+disp(encoded_signal);
 end
 
